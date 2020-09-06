58d23605db127d8e1a24b3c5867a2c3eb0cf5dd4
xcode <gricebrett@GMAIl.com>
2020-09-06 12:40:03 -0500

    adding my playground file

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var coolwords = "hello, playground"
+
+var integer = 10
+var doubleTester: Double = 10.053242
+var string2 = "Hi there!"
+var string3 = "Strings always exist in quoutes"
+var integer2 = 20
+
+coolwords
+
+integer2
+integer + integer2
+integer * integer2
+integer / integer2
+coolwords + string2
+
+coolwords + String(integer)
